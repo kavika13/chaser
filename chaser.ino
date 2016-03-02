85d8503a854ddc5a04b6f8e1b43df97c1df0cc0f
cconstantine <cconstan@gmail.com>
2016-03-01 23:12:49 -0800

    add fire effect

--- chaser.ino
+++ chaser.ino
@@ -40,6 +40,7 @@ void setup() {
   radio.openReadingPipe(1, pipes[0]);
   radio.startListening();                 // Start listening
   radio.printDetails();                   // Dump the configuration of the rf unit for debugging
+  
 }
 
 void loop(void) {
@@ -68,7 +69,11 @@ void loop(void) {
     printf("At: 0x%0.4x, delta: %d, offset: %d\n", synced_time(), delta, offset);
     offset += correction;
     delta -= correction;
-    rainbow();
+    if (cycle(256, 0) > 128) {
+      rainbow();
+    } else {
+      fire();
+    }
     FastLED.show();
   }
 
@@ -82,6 +87,61 @@ void loop(void) {
   }
 }
 
+// COOLING: How much does the air cool as it rises?
+// Less cooling = taller flames.  More cooling = shorter flames.
+// Default 55, suggested range 20-100 
+#define COOLING  70
+
+// SPARKING: What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire.  Lower chance = more flickery fire.
+// Default 120, suggested range 50-200.
+#define SPARKING 60
+
+void fire()
+{
+  fadeToBlackBy(leds, NUM_LEDS, 20);
+
+  uint16_t now = cycle(255, 0);
+  static uint16_t lastAt = now;
+  if (now < lastAt) {
+    random16_set_seed(0);
+  }
+  lastAt = now;
+    
+  uint8_t hue = cycle(255, 3);
+  hue++;
+  CRGB darkcolor  = CHSV(hue,255,192); // pure hue, three-quarters brightness
+  CRGB lightcolor = CHSV(hue,128,255); // half 'whitened', full brightness
+  CRGBPalette16 gPal = CRGBPalette16( CRGB::Black, darkcolor, lightcolor, CRGB::White);
+ 
+  // Array of temperature readings at each simulation cell
+  static byte heat[NUM_LEDS];
+
+  // Step 1.  Cool down every cell a little
+    for( int i = 0; i < NUM_LEDS; i++) {
+      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    }
+  
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for( int k= NUM_LEDS - 1; k >= 2; k--) {
+      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    }
+    
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if( random8() < SPARKING ) {
+      int y = random8(7);
+      heat[y] = qadd8( heat[y], random8(160,255) );
+    }
+
+    // Step 4.  Map from heat cells to LED colors
+    for( int j = 0; j < NUM_LEDS; j++) {
+      // Scale the heat value from 0-255 down to 0-240
+      // for best results with color palettes.
+      byte colorindex = scale8( heat[j], 240);
+
+      leds[j] = leds[j] + ColorFromPalette( gPal, colorindex);
+    }
+}
 
 void rainbow()
 {
@@ -99,9 +159,6 @@ void rainbow()
   fill_rainbow(&color, 1,  cycle(256, 5), 12);
   leds[cycle_center] = leds[cycle_center] + color;
 }
-
-
-
 
 uint16_t synced_time() {
   uint16_t cycle_size = 0xFFFF;
